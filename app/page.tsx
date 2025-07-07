eb719b23d84f4697f516522eacc2b4787e0ced1a
Aniket De <aniketde1234@gmail.com>
2025-07-08 02:23:39 +0530

    Random Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Header from "./dashboard/_components/Header";
 import Link from "next/link";
 
 export default function Home() {
